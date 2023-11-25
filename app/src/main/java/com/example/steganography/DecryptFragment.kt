c46c444444501ceae257436ddcc9001379ee417d
Saksham <saksham.varma02@gmail.com>
2023-11-26 02:41:16 +0530

    encrypt and decrypt ui done image picker done

--- app/src/main/java/com/example/steganography/DecryptFragment.kt
+++ app/src/main/java/com/example/steganography/DecryptFragment.kt
@@ -1,12 +1,17 @@
 package com.example.steganography
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.steganography.databinding.FragmentDecryptBinding
-
+import java.io.FileDescriptor
+import java.io.IOException
 
 class DecryptFragment : Fragment() {
     private var _binding: FragmentDecryptBinding? = null
@@ -24,11 +29,37 @@ class DecryptFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentDecryptBinding.inflate(inflater, container, false)
 
+        val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            val galleryUri = it
+            try{
+                val bitmap = uriToBitmap(galleryUri!!)
+                binding.imageView.setImageBitmap(bitmap)
+                binding.TextViewTextMultiLine.text = bitmap.toString()
+            }catch(e:Exception){
+                e.printStackTrace()
+            }
 
+        }
 
-
+        binding.selectImageButton.setOnClickListener {
+            galleryLauncher.launch("image/*")
+        }
 
         return binding.root
+    }
+
+    private fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
+        try {
+            val parcelFileDescriptor =
+                context?.contentResolver?.openFileDescriptor(selectedFileUri, "r")
+            val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+            val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+            parcelFileDescriptor.close()
+            return image
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
     }
 
 
--- app/src/main/java/com/example/steganography/EncryptFragment.kt
+++ app/src/main/java/com/example/steganography/EncryptFragment.kt
@@ -1,12 +1,17 @@
 package com.example.steganography
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.steganography.databinding.FragmentEncryptBinding
-
+import java.io.FileDescriptor
+import java.io.IOException
 
 class EncryptFragment : Fragment() {
     private var _binding: FragmentEncryptBinding? = null
@@ -24,10 +29,36 @@ class EncryptFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentEncryptBinding.inflate(inflater, container, false)
 
+        val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            val galleryUri = it
+            try{
+                val bitmap = uriToBitmap(galleryUri!!)
+                binding.imageView.setImageBitmap(bitmap)
+            }catch(e:Exception){
+                e.printStackTrace()
+            }
 
+        }
 
+        binding.selectImageButton.setOnClickListener {
+            galleryLauncher.launch("image/*")
 
+        }
         return binding.root
+    }
+
+    private fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
+        try {
+            val parcelFileDescriptor =
+                context?.contentResolver?.openFileDescriptor(selectedFileUri, "r")
+            val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+            val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+            parcelFileDescriptor.close()
+            return image
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
     }
 
 }
